2c452cf52d78b9686f781bb684a8d3a2731f7c78
1274369102 <1274369102@qq.com>
2019-12-24 15:47:05 +0800

    黑马头条PC-主页模块-头部信息查询

--- src/components/layout-header.vue
+++ src/components/layout-header.vue
@@ -10,10 +10,10 @@
     <!-- 右侧 -->
     <el-col class="right" :span="4">
       <el-row type="flex" justify="ned" align="middle">
-        <img src="../assets/img/avatar.jpg" alt />
+        <img :src="userInfo.photo ? userInfo.photo:defaultImg" alt="" />
         <!-- 下拉菜单 -->
         <el-dropdown>
-          <span>反动神</span>
+          <span>{{ userInfo.name }}</span>
           <!-- 下拉菜单  具名插槽 -->
           <el-dropdown-menu slot="dropdown">
               <!-- 下拉内容 -->
@@ -28,7 +28,27 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {}, // 用户信息
+      defaultImg: require('../assets/img/fds.jpg')//  先把地址转换成变量
+    }
+  },
+  created () {
+    let token = window.localStorage.getItem('user-token')// 获取令牌
+    // 查询数据
+    this.$axios({
+      url: '/user/profile',
+
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(result => {
+      this.userInfo = result.data.data// 获取用户个人信息
+    })
+  }
+}
 </script>
 
 <style lang='less' scoped>
