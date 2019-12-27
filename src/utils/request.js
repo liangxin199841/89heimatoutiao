f6582189b2d994aab13a594b0c6bbc6e1bcc6c05
1274369102 <1274369102@qq.com>
2019-12-27 17:42:00 +0800

    黑马头条PC--接口访问-axios拦截器-统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 // 对axios进行一个封装
 import axios from 'axios'
+import router from '../router'// 路由实例对象引入
+import { Message } from 'element-ui'
 // 请求拦截器
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 赋值黑马头条的默认地址
 // 请求拦截
@@ -15,7 +17,36 @@ axios.interceptors.request.use(function (config) {
 // 响应拦截
 axios.interceptors.response.use(function (response) {
   return response.data ? response.data : {}// 解决当data不存在时 then中读取数据报错问题
-}, function () {
+}, function (error) {
+  // 回调函数 所有的失败 都会进入到第二个回调函数
+  // 失败时执行 状态码 不是200 或者201/204
+  // 获取状态码 => 根据状态码 进行相应的提示或者操作
+  let status = error.response.status// 获取状态码 根据状态码进行提示
 
+  let message = ''// 提示信息
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      // token 过期 强制跳转到登录页
+      // this.$router=>路由实例对象
+      router.push('/login')
+      break
+    case 401:
+      // token过期
+      router.push('/login')// 强制回登陆页
+      break
+    case 507:
+      message = '服务器数据库异常'
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  // 状态码提示
+  Message({ type: 'warning', message })
 })
 export default axios
