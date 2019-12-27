01cb7f80ddd1592c1a05ada622a46c2a683592cf
1274369102 <1274369102@qq.com>
2019-12-27 17:58:12 +0800

    黑马头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import LayoutAside from './layout-aside'
 import LayoutHeader from './layout-header'
+import BreadCrumb from './common/bread-crumb'
 export default {
   install (vue) {
     vue.component('layout-aside', LayoutAside)// 全局注册
     vue.component('layout-header', LayoutHeader)// 全局注册
+    vue.component('bread-crumb', BreadCrumb)
   }
 }
